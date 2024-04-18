1a1121be209357fccb5967df65efc1d6aa5d0fd1
AgSilvers47 <167281464+AgSilvers47@users.noreply.github.com>
2024-04-18 20:46:17 +0800

    fourth commit - fixed the operation

--- newfile.py
+++ newfile.py
@@ -30,12 +30,12 @@ while True:
 			continue
 		
 		if choice == '1':
-			print(n1, "+", n2, "=", add(n1, n2))
+			print(n1, "+", n2, "=", (n1+n2))
 		elif choice == '2':
-			print(n1, "-", n2, "=", subtract(n1, n2))
+			print(n1, "-", n2, "=", (n1-n2))
 		elif choice == '3':
-			print(n1, "*", n2, "=", multiply(n1, n2))
+			print(n1, "*", n2, "=", (n1*n2))
 		elif choice == '4':
-			print(n1, "/", n2, "=", divide(n1, n2))
+			print(n1, "/", n2, "=", (n1/n2))
 		elif choice == '5':
-			print(n1, "**", n2, "=", power(n1, n2))
+			print(n1, "**", n2, "=", (n1**n2))
